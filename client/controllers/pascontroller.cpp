3156ff06253ba1da60deb4878ac65615b149b5a3
bryankim96 <bryan.sanghyuk.kim@gmail.com>
2019-05-08 14:50:44 -0700

    Changes to addDevice() logging for clarity.

--- client/controllers/pascontroller.cpp
+++ client/controllers/pascontroller.cpp
@@ -1,6 +1,6 @@
 #include "pascontroller.hpp"
 #include "ccdclass.h"
-#include "clienthelper.hpp"
+#include "client/clienthelper.hpp"
 #include "components.hpp"
 #include "common/opcua/pasobject.hpp"
 #include <iostream>
@@ -22,22 +22,19 @@
 // implement PasCompositeController::addChild()
 void PasCompositeController::addChild(OpcUa_UInt32 deviceType, PasController *const pController)
 {
-    std::cout << " --- " << m_ID.name << "::addChild(): "
-              << m_ID.name << ": adding " << pController->getId() << std::endl;
-
     const auto &id = pController->getId();
     auto pos = pController->getId().position;
     // don't add the same device multiple times
     try {
         // m_ChildrenPositionMap.at(deviceType).at(pos);
         m_ChildrenIdentityMap.at(deviceType).at(id);
-        cout << "\tAlready added. Moving on..." << endl;
-
         return;
     }
     catch (std::out_of_range &e) {
         // only add if this is a possible child
         if (m_ChildrenTypes.count(deviceType)) {
+            std::cout << " --- " << m_ID.name << "::addChild(): "
+                      << m_ID.name << ": adding " << pController->getId() << std::endl;
             m_pChildren[deviceType].push_back(pController);
             m_ChildrenIdentityMap[deviceType][id] = m_pChildren.at(deviceType).size() - 1;
             // this doesn't work for edges, since they don't have an assigned position
