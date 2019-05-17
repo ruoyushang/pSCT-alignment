5de8793566d5539da6765c651d250f2bb2f1a4b0
pSCT Admin <ctauser@alignment.psct>
2019-05-17 15:15:47 -0700

    Added some scripts to log currents of panels while using, and updated filenames to panel lists

---
+++ tools/start_current_logging.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env bash
+
+panel_list=$1
+for pos in `cat ~/repos/bryan/pSCT-alignment/misc/panel_lists/$panel_list`; do 
+    ~/p2pas/passerver_tools/get_panel_current.sh $pos & 
+done
