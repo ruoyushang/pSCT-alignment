842c7bf2fbc5e0f31a202b2745279058b0cd524a
user <user@localhost.localdomain>
2020-03-19 12:29:18 -0700

    define m_PSDIdentityMap

--- common/alignment/platform.hpp
+++ common/alignment/platform.hpp
@@ -77,14 +77,6 @@ public:
         return m_MPES.at(m_MPESIdentityMap.at(identity));
     }
 
-    int getPSDCount() const { return m_PSD.size(); }
-
-    std::unique_ptr<Device> &getPSD(int idx) { return m_PSD.at(idx); }
-
-    std::unique_ptr<Device> &getPSDbyIdentity(const Device::Identity &identity) {
-        return m_PSD.at(m_PSDIdentityMap.at(identity));
-    }
-
     // Actuator-related methods
     void probeEndStopAll(int direction);
 
